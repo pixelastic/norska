b7535baa1600730736423d68e5ce88c02b52b5e6
Pixelastic <tim@pixelastic.com>
2019-07-17 10:07:09 +0200

    feat(scripts): Add convenience script to update deps

--- scripts/meta/update-dependencies.js
+++ scripts/meta/update-dependencies.js
@@ -1,27 +1,33 @@
 import firost from 'firost';
-import path from 'path';
 import { _, pMap } from 'golgoth';
 
 // Those modules will be updated to the latest available version
 const safelist = ['firost', 'aberlaas', 'golgoth'];
 
 (async function() {
-  // // Remove and re-add those packages from devDependencies in the root
-  // const commandRemove = `yarn remove -W --dev ${safelist.join(' ')}`;
-  // const commandAdd = `yarn add -W --dev ${safelist.join(' ')}`;
-  // console.info("Removing dependencies from root");
-  // await firost.shell(commandRemove);
-  // console.info("Re-adding dependencies from root");
-  // await firost.shell(commandAdd);
+  // Remove and re-add those packages from devDependencies in the root
+  const commandRemove = `yarn remove -W ${safelist.join(' ')}`;
+  const commandAdd = `yarn add -W --dev ${safelist.join(' ')}`;
+  console.info('Removing dependencies from root');
+  await firost.shell(commandRemove);
+  console.info('Re-adding dependencies to root');
+  await firost.shell(commandAdd);
 
   // Find the real versions to use
   const { devDependencies } = await firost.readJson('package.json');
   const versions = _.pick(devDependencies, safelist);
+  console.info('Correct versions');
   console.info(versions);
 
   // Find all the package.json from all modules
   const filepaths = await firost.glob('./modules/*/package.json');
 
+  /**
+   * Update values of a dependency object with values passed in second argument
+   * @param {object} dependencies Dependency object from a package.json
+   * @param {object} correctVersions Object of module: version
+   * @returns {object} Updated dependency object
+   **/
   function updateDependencies(dependencies, correctVersions) {
     return _.mapValues(dependencies, (value, key) => {
       if (correctVersions[key]) {
@@ -34,56 +40,17 @@ const safelist = ['firost', 'aberlaas', 'golgoth'];
   // Update all those package.json with the new versions
   await pMap(filepaths, async filepath => {
     const packageJson = await firost.readJson(filepath);
-    packageJson.dependencies = updateDependencies(packageJson.dependencies, versions);
+    packageJson.dependencies = updateDependencies(
+      packageJson.dependencies,
+      versions
+    );
     if (packageJson.devDependencies) {
       packageJson.devDependencies = updateDependencies(
-        packageJson.devDependencies, versions
+        packageJson.devDependencies,
+        versions
       );
     }
 
-    console.info(packageJson);
+    await firost.writeJson(packageJson, filepath, { sort: false });
   });
-  // const result = await pMap(filepaths, async filepath => {
-  //   const moduleName = path.basename(path.dirname(filepath));
-
-  //   const content = await firost.readJson(filepath);
-  //   const packagesToUpdate = [];
-  //   _.each(_.pick(content.dependencies, safelist), (value, key) => {
-  //     packagesToUpdate.push({
-  //       isDevDependency: false,
-  //       packageName: key,
-  //     });
-  //   });
-  //   _.each(_.pick(content.devDependencies, safelist), (value, key) => {
-  //     packagesToUpdate.push({
-  //       isDevDependency: true,
-  //       packageName: key,
-  //     });
-  //   });
-  //   return {
-  //     moduleName,
-  //     packagesToUpdate,
-  //   };
-  // });
-
-  // // Go to each module, delete the dependency and add it again
-  // await pMap(result, async item => {
-  //   const commands = [`cd ./modules/${item.moduleName}`]
-  //   if (item.isDevDependency) {
-  //     commands.push['yarn remove --dev ${ packageName
-  //   }
-  //   await firost.shell(`cd ./modules/${item.moduleName} && yarn remove
-  // });
-
-  // console.info(result[3]);
 })();
-
-// #!/usr/bin/env sh
-// # yarn upgrade --latest does not seem to really work with 0.x version
-// # Best way to update firost/golgoth is to:
-// # - Loop through each package.json
-// # - If they have firost or golgoth in their dependency, they need to update
-// # - We build an array of all deps found
-// # - We run yarn remove firost && yarn add firost for eahc, adding --save-dev if
-// # in devDeps
-// set -e
