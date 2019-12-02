7c34450add67cfa132ce75c64d261a346bbc72c5
Pixelastic <tim@pixelastic.com>
2019-12-02 17:44:38 +0100

    style(lint): Ignore import errors in templates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 /* eslint-disable import/no-commonjs */
 module.exports = {
   extends: ['./node_modules/aberlaas/build/configs/eslint.js'],
+  overrides: [
+    {
+      // Ignore require/import errors in templates
+      files: ['./modules/norska-init/templates/*.js'],
+      rules: {
+        'import/no-extraneous-dependencies': 0,
+      },
+    },
+  ],
 };
