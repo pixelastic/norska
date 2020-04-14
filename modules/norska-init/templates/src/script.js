129edb10647dc80755ffda9c53f02a75d7915c58
Pixelastic <tim@pixelastic.com>
2020-04-14 14:50:08 +0200

    feat(frontend): Publish frontend/ in norska

--- modules/norska-init/templates/src/script.js
+++ modules/norska-init/templates/src/script.js
@@ -1,5 +1,4 @@
 // Lazy loading of images when in viewport
 // <img data-src="image.png" class="lazyload" />
-// <div data-bg="image.png" class="bg-contain lazyload"></div>
-const lazyload = require('norska-frontend/lib/lazyload');
+const lazyload = require('norska/frontend/lazyload');
 lazyload.init();
