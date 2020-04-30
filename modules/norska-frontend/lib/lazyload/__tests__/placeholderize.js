83f795400f509fbc4708e8437194127615c80364
Pixelastic <tim@pixelastic.com>
2020-04-30 23:33:31 +0200

    fix(img): Do not resize placeholders

--- modules/norska-frontend/lib/lazyload/__tests__/placeholderize.js
+++ modules/norska-frontend/lib/lazyload/__tests__/placeholderize.js
@@ -7,32 +7,12 @@ describe('norska-frontend > lazyload > placeholderize', () => {
   });
   it.each([
     // Object | String
-    [{}, 'e_blur:300,f_auto,q_10,w_0.33'],
-    [{ placeholder: { opacity: 50 } }, 'e_blur:300,f_auto,o_50,q_10,w_0.33'],
-    [{ placeholder: { width: 500 } }, 'e_blur:300,f_auto,q_10,w_500'],
-    [{ placeholder: { height: 500 } }, 'e_blur:300,f_auto,h_500,q_10'],
-    [
-      { placeholder: { width: 300, height: 500 } },
-      'e_blur:300,f_auto,h_500,q_10,w_300',
-    ],
-    [{ opacity: 50 }, 'e_blur:300,f_auto,o_50,q_10,w_0.33'],
-    [{ format: 'png' }, 'e_blur:300,f_png,q_10,w_0.33'],
-    // Dimensions are relative to the main image
-    [{ width: 300 }, 'e_blur:300,f_auto,q_10,w_100'],
-    [{ height: 300 }, 'e_blur:300,f_auto,h_100,q_10'],
-    [{ width: 300, height: 300 }, 'e_blur:300,f_auto,h_100,q_10,w_100'],
-    [
-      { width: 300, height: 300, placeholder: { width: 42, height: 42 } },
-      'e_blur:300,f_auto,h_42,q_10,w_42',
-    ],
-    [
-      { width: 300, height: 300, placeholder: { width: 42 } },
-      'e_blur:300,f_auto,h_100,q_10,w_42',
-    ],
-    [{ width: 0.5, height: 0.5 }, 'e_blur:300,f_auto,h_0.17,q_10,w_0.17'],
-    // Quality is always 10, unless specifically changed
-    [{ quality: 80 }, 'e_blur:300,f_auto,q_10,w_0.33'],
-    [{ placeholder: { quality: 80 } }, 'e_blur:300,f_auto,q_80,w_0.33'],
+    [{}, 'e_blur:300,f_auto,q_auto:low'],
+    [{ placeholder: { opacity: 50 } }, 'e_blur:300,f_auto,o_50,q_auto:low'],
+    [{ opacity: 50 }, 'e_blur:300,f_auto,o_50,q_auto:low'],
+    [{ format: 'png' }, 'e_blur:300,f_png,q_auto:low'],
+    [{ quality: 80 }, 'e_blur:300,f_auto,q_auto:low'],
+    [{ placeholder: { quality: 80 } }, 'e_blur:300,f_auto,q_80'],
   ])('%s => %s', async (options, params) => {
     const input = 'http://www.example.com/foo.png';
     const expected = `https://res.cloudinary.com/bucket-foo/image/fetch/${params}/http%3A%2F%2Fwww.example.com%2Ffoo.png`;
--- modules/norska-frontend/lib/lazyload/placeholderize.js
+++ modules/norska-frontend/lib/lazyload/placeholderize.js
@@ -1,31 +1,6 @@
 const proxy = require('../cloudinary/proxy');
 
 /**
- * Define scaled down dimensions
- * We use any dimensions specified by the placeholder first
- * Otherwise we manually scale down what is passed to the main image
- * If nothing is passed, we let Cloudinary resize
- * @param {object} options Cloudinary options for the main image
- * @param {string} key Key to transform (width/height)
- * @returns {number} New dimension
- **/
-function scaleDown(options, key) {
-  // Specific placeholder dimensions is set, we use it
-  if (options.placeholder && options.placeholder[key]) {
-    return options.placeholder[key];
-  }
-
-  // No value specified, we ignore it
-  const baseValue = options[key];
-  if (!baseValue) {
-    return false;
-  }
-
-  // Value specified, we manually divide it
-  const isRelative = baseValue < 1;
-  return isRelative ? (baseValue / 3).toFixed(2) : Math.floor(baseValue / 3);
-}
-/**
  * Returns a placeholder version of an image, to be used for lazyloading
  * @param {string} url Initial image url
  * @param {object} userOptions Cloudinary options. See
@@ -36,29 +11,11 @@ function scaleDown(options, key) {
 module.exports = function(url, userOptions) {
   const placeholderOptions = userOptions.placeholder || {};
 
-  // Get actual width and height value
-  // We pass them if defined for the placeholder, or we derive them from the
-  // main value. If none are set, we only set the width
-  const width = scaleDown(userOptions, 'width');
-  const height = scaleDown(userOptions, 'height');
-  const dimensions = {};
-  if (!width && !height) {
-    dimensions.width = '0.33';
-  }
-  if (width) {
-    dimensions.width = width;
-  }
-  if (height) {
-    dimensions.height = height;
-  }
-
   const options = {
     blur: 300,
     ...userOptions,
-    quality: 10,
+    quality: 'auto:low',
     ...placeholderOptions,
-    ...dimensions,
-    placeholder: null,
   };
   return proxy(url, options);
 };
