b1be29ba62cfb8a6cb19206a5d49e8a6b1b273c8
Pixelastic <tim@pixelastic.com>
2020-05-05 17:53:38 +0200

    fix(css): Actually move purge removal into tailwind-config-norska

--- modules/norska-css/lib/tailwind.config.js
+++ modules/norska-css/lib/tailwind.config.js
@@ -1,7 +1,2 @@
 const config = require('tailwind-config-norska');
-
-// Specifically disable the builtin purge inside Tailwind, because we already
-// purge as part of our build
-config.purge = false;
-
 module.exports = config;
