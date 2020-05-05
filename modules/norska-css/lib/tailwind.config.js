2b892698b91a9bd57a8bbbbcef0cf5e1b5ff2742
Pixelastic <tim@pixelastic.com>
2020-05-05 17:52:36 +0200

    feat(css): Disable builtin purge in tailwindcss

--- modules/norska-css/lib/tailwind.config.js
+++ modules/norska-css/lib/tailwind.config.js
@@ -1,2 +1,7 @@
 const config = require('tailwind-config-norska');
+
+// Specifically disable the builtin purge inside Tailwind, because we already
+// purge as part of our build
+config.purge = false;
+
 module.exports = config;
