c8c4296bd45b807b2dd07acc5b6b91500cad5dbf
Tim Carry <tim@pixelastic.com>
2021-04-11 16:14:34 +0200

    feat(serve): Add working livereload server

--- modules/serve/lib/__tests__/main.js
+++ modules/serve/lib/__tests__/main.js
@@ -3,8 +3,16 @@ const assets = require('norska-assets');
 const js = require('norska-js');
 const css = require('norska-css');
 const html = require('norska-html');
+const config = require('norska-config');
+const emptyDir = require('firost/emptyDir');
+const readUrl = require('firost/readUrl');
+const write = require('firost/write');
+const getPort = require('get-port');
 
 describe('norska-serve', () => {
+  const tmpDirectory = './tmp/norska-serve';
+  let staticServerUrl;
+
   describe('watchFiles', () => {
     it('should listen to changes', async () => {
       jest.spyOn(html, 'watch').mockReturnValue();
@@ -18,6 +26,67 @@ describe('norska-serve', () => {
       expect(css.watch).toHaveBeenCalled();
       expect(js.watch).toHaveBeenCalled();
       expect(assets.watch).toHaveBeenCalled();
+    });
+  });
+  describe('startStaticServer', () => {
+    beforeEach(async () => {
+      await emptyDir(tmpDirectory);
+
+      const staticServerPort = await getPort();
+      staticServerUrl = `http://127.0.0.1:${staticServerPort}`;
+      await config.init({
+        from: `${tmpDirectory}/src`,
+        to: `${tmpDirectory}/dist`,
+        port: staticServerPort,
+      });
+
+      jest.spyOn(current, '__consoleInfo').mockReturnValue();
+      jest.spyOn(current, '__open').mockReturnValue();
+    });
+    afterEach(async () => {
+      await current.closeStaticServer();
+    });
+    it('should serve files from the dist folder', async () => {
+      await write('some content', config.toPath('index.html'));
+
+      await current.startStaticServer();
+
+      const actual = await readUrl(staticServerUrl);
+      expect(actual).toEqual('some content');
+    });
+    it('should serve fresh content', async () => {
+      await write('some content', config.toPath('index.html'));
+      await current.startStaticServer();
+
+      const firstRead = await readUrl(staticServerUrl);
+      expect(firstRead).toEqual('some content');
+
+      await write('updated content', config.toPath('index.html'));
+      const secondRead = await readUrl(staticServerUrl, { memoryCache: false });
+      expect(secondRead).toEqual('updated content');
+    });
+    it('should serve content added after server starts', async () => {
+      await current.startStaticServer();
+
+      await write('some content', config.toPath('index.html'));
+
+      const actual = await readUrl(staticServerUrl);
+      expect(actual).toEqual('some content');
+    });
+    it('should add liveReload script to html pages', async () => {
+      await write(
+        '<!DOCTYPE html><html><body>some content</body></html>',
+        config.toPath('index.html')
+      );
+
+      await current.startStaticServer();
+
+      const actual = await readUrl(staticServerUrl, {
+        headers: { Accept: 'text/html' },
+      });
+      expect(actual).toInclude(
+        '<script src="//127.0.0.1:35729/livereload.js?snipver=1"'
+      );
     });
   });
 });
--- modules/serve/lib/main.js
+++ modules/serve/lib/main.js
@@ -8,13 +8,21 @@ const express = require('express');
 const open = require('open');
 const livereload = require('livereload');
 const connectLivereload = require('connect-livereload');
+const consoleInfo = require('firost/consoleInfo');
 
 module.exports = {
+  /**
+   * Start live reload server.
+   * Listen to file changes, serve them locally, and refresh browser on change
+   **/
   async run() {
     await this.watchFiles();
     await this.startStaticServer();
     await this.startLivereloadServer();
   },
+  /**
+   * Watch for changes in any source file and rebuild them in destination folder
+   **/
   async watchFiles() {
     await pAll([
       async () => await html.watch(),
@@ -22,6 +30,44 @@ module.exports = {
       async () => await js.watch(),
       async () => await assets.watch(),
     ]);
+  },
+  /**
+   * Start a server rendering any file located in destination folder
+   * @returns {Promise} Resolves when the server is started
+   **/
+  async startStaticServer() {
+    return new Promise((resolve, _reject) => {
+      const app = express();
+
+      // Add the livereload.js script to the pages
+      app.use(connectLivereload());
+
+      app.use(express.static(config.toPath()));
+
+      // Start the server
+      const cmsPort = config.get('port');
+      this.__staticServer = app.listen(cmsPort, async () => {
+        const serverUrl = `http://127.0.0.1:${cmsPort}/`;
+        this.__consoleInfo(`Dev server available at ${serverUrl}`);
+        await this.__open(serverUrl);
+        resolve();
+      });
+    });
+  },
+  /**
+   * Close the previously opened static server
+   * @returns {Promise} Resolve when the server is closed
+   **/
+  async closeStaticServer() {
+    if (!this.__staticServer) {
+      return;
+    }
+    // We give it the order to close, but only resolve once it is closed for
+    // real
+    return await new Promise((resolve) => {
+      this.__staticServer.on('close', resolve);
+      this.__staticServer.close();
+    });
   },
   async startLivereloadServer() {
     const livereloadOptions = {
@@ -33,25 +79,6 @@ module.exports = {
       .createServer(livereloadOptions)
       .watch(watchedDirectories);
   },
-  async startStaticServer() {
-    return new Promise((resolve, _reject) => {
-      const app = express();
-
-      // Add the livereload.js script to the pages
-      app.use(connectLivereload());
-
-      app.use(express.static(config.to()));
-
-      // Start the server
-      const cmsPort = config.get('port');
-      app.listen(cmsPort, async () => {
-        const cmsUrl = `http://127.0.0.1:${cmsPort}/`;
-        console.info(`Dev server available at ${cmsUrl}`);
-        await open(cmsUrl);
-        resolve();
-      });
-    });
-  },
   /**
    * Wrapping the livereload dependency so we can mock it in tests
    * @returns {object} Livereload object
@@ -59,4 +86,7 @@ module.exports = {
   __livereload() {
     return livereload;
   },
+  __open: open,
+  __server: null,
+  __consoleInfo: consoleInfo,
 };
